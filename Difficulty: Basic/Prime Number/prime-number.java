0746a2a7e52f816a505dd72896474d19afd1529a
VIKAS D <122000473+vikasdinesh17@users.noreply.github.com>
2025-06-27 20:51:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Prime Number/prime-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    static boolean isPrime(int n) {
+        if(n==1){
+             return false;
+         }else{
+     for(int i=2;i*i<=n;i++){
+          if(n%i==0){
+             return false;
+         }
+     }
+     return true;
+    }
+}
+}
